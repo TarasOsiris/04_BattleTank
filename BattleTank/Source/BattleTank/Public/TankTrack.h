bb70ad476f0415d88ef3fee70c65328502b02c91
Taras Leskiv <leskiv.taras@gmail.com>
2018-02-18 14:20:31 +0100

    156

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -18,5 +18,6 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
 	
-	
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 40000.0f; // in Newtons
 };
