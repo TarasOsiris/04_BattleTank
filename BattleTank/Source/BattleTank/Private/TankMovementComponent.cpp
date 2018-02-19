80d795419a176cae61d947f2b3f84b5411abfcf0
Taras Leskiv <leskiv.taras@gmail.com>
2018-02-19 23:57:14 +0100

    161

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -6,18 +6,28 @@
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Intend to move forward: %f"), Throw);
-	LeftTrack->SetThrottle(Throw);
-	RightTrack->SetThrottle(Throw);
-}
-
-void UTankMovementComponent::Initialize(UTankTrack * LeftTrackToSet, UTankTrack * RightTrackToSet)
-{
-	if (!LeftTrackToSet || !RightTrackToSet)
+	if (!LeftTrack || !RightTrack)
 	{
 		return;
 	}
 
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+}
+
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+	if (!LeftTrack || !RightTrack)
+	{
+		return;
+	}
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
+}
+
+void UTankMovementComponent::Initialize(UTankTrack * LeftTrackToSet, UTankTrack * RightTrackToSet)
+{
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
 }
