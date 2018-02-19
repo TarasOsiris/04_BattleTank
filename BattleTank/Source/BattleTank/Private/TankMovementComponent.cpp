5ea541861c2febed188b48bc72af88d61310bedd
Taras Leskiv <leskiv.taras@gmail.com>
2018-02-19 23:41:54 +0100

    160

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,11 +1,23 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
 #include "Runtime/Engine/Classes/Engine/World.h"
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	UE_LOG(LogTemp, Warning, TEXT("Intend to move forward: %f"), Throw);
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
 }
 
+void UTankMovementComponent::Initialize(UTankTrack * LeftTrackToSet, UTankTrack * RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet)
+	{
+		return;
+	}
 
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
